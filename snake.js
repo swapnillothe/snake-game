2aa2c7b4bc8328fbbf5535ba3f7ff8dca9e070ec
swapnil lothe <44019530+swapnillothe@users.noreply.github.com>
2019-01-05 01:02:07 +0530

    for multiple body parts its working

--- snake.js
+++ snake.js
@@ -18,6 +18,7 @@ let position = { x: 100, y: 100 };
 let obsPosition = { x: 400, y: 300 };
 let movement = 'x';
 let action = addTen;
+let length = 2;
 let snake = [{ action, movement, position }];
 
 const getPositionTag = function (position) {
@@ -29,43 +30,53 @@ const moveSnakeObs = function () {
   obstacle.setAttribute('style', `${getPositionTag(obsPosition)};`);
 }
 
+
+const getLastPart = function () {
+  let action = snake[snake.length - 1]['action'];
+  let movement = snake[snake.length - 1]['movement'];
+  let position = {};
+  position['x'] = snake[snake.length - 1]['position']['x'];
+  position['y'] = snake[snake.length - 1]['position']['y'];
+  return { action, movement, position };
+}
+
+const addTail = function () {
+  snake.push(getLastPart());
+}
+
+const addTailBodyTag = function () {
+  let id = length++;
+  return `<div id=${id} class='snakeBody'></div>`
+}
+
+const addTailBody = function () {
+  let snake = document.getElementById('snake');
+  snake.innerHTML += `${addTailBodyTag()}`;
+  addTail();
+}
+
+const moveSnakeBody = function () {
+  for (let index = snake.length - 1; index > 1; index--) {
+    snake[index]['action'] = snake[index - 1]['action'];
+    if (snake[index - 1]['action'] == addTen) {
+      snake[index]['action'] = increaseByTen;
+    }
+    if (snake[index - 1]['action'] == subtractTen) {
+      snake[index]['action'] = decreaseByTen;
+    }
+    snake[index]['movement'] = snake[index - 1]['movement'];
+    snake[index]['position']['x'] = snake[index - 1]['position']['x'];
+    snake[index]['position']['y'] = snake[index - 1]['position']['y'];
+    let head = document.getElementById(index);
+    position[movement] = action(position[movement]);
+    head.setAttribute('style', `${getPositionTag(position)};`);
+  }
+}
+
 const moveSnakeHead = function () {
   let head = document.getElementById(1);
   position[movement] = action(position[movement]);
   head.setAttribute('style', `${getPositionTag(position)};`);
-}
-
-const moveSnakeBody = function () {
-  let body1 = document.getElementById(2);
-  position[movement] = action(position[movement]);
-  body1.setAttribute('style', `${getPositionTag(position)};`);
-
-  let body2 = document.getElementById(3);
-  position[movement] = action(position[movement]);
-  body2.setAttribute('style', `${getPositionTag(position)};`);
-
-  let body3 = document.getElementById(4);
-  position[movement] = action(position[movement]);
-  body3.setAttribute('style', `${getPositionTag(position)};`);
-
-  let body4 = document.getElementById(5);
-  position[movement] = action(position[movement]);
-  body4.setAttribute('style', `${getPositionTag(position)};`);
-
-  let body5 = document.getElementById(6);
-  position[movement] = action(position[movement]);
-  body5.setAttribute('style', `${getPositionTag(position)};`);
-
-  let body6 = document.getElementById(7);
-  position[movement] = action(position[movement]);
-  body6.setAttribute('style', `${getPositionTag(position)};`);
-
-  for (let index = snake.length - 1; index > 0; index--) {
-    snake[index]['action'] = snake[index - 1]['action'];
-    snake[index]['movement'] = snake[index - 1]['movement'];
-    snake[index]['position']['x'] = snake[index - 1]['position']['x'];
-    snake[index]['position']['y'] = snake[index - 1]['position']['y'];
-  }
 }
 
 const moveSnake = function () {
@@ -108,4 +119,7 @@ const updateMovement = function (event) {
 
 setInterval(() => {
   moveSnake();
-}, 400);
+  if (snake[0]['position']['x'] == obsPosition['x']) {
+    addTailBody();
+  }
+}, 300);
